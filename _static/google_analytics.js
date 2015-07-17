23b83f449036223d7b15937fda3beb3e98d31553
jetgeng <jetgeng@gmail.com>
2015-07-17 16:57:05 +0800

    删除所有不在使用的内容

--- _static/google_analytics.js
+++
@@ -1,4 +0,0 @@
-<script type="text/javascript">
-var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F9e32b7b0f98239ffd9d0d375110a8d72' type='text/javascript'%3E%3C/script%3E"));
-</script>
